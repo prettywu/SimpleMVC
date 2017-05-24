1f4d7cdf84fea8716ce20655c0154a39af424fff
不许睡、起来嗨 <838622886@qq.com>
2017-05-24 18:17:09 +0800

    模板测试

--- SimpleMVC/App_Start/BundleConfig.cs
+++ SimpleMVC/App_Start/BundleConfig.cs
@@ -20,8 +20,7 @@ namespace SimpleMVC.App_Start
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
